f11aa5f2175d014579f7c410714ca25e970c82a6
Krish Gupta <krshg@outlook.com>
2023-03-05 08:00:33 +0000

    fix: failing deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
 import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
   integrations: [tailwind()]
 });
